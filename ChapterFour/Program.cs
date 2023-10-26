2ee3e5acc9427347ee3f2ab8f828b5e2cbe42c40
Abdulraheem Adebare <abdulraheemadebare55@gmail.com>
2023-10-26 16:29:38 +0100

    Did some more exercices

--- ChapterFour/Program.cs
+++ ChapterFour/Program.cs
@@ -1,36 +1,90 @@
-﻿// Number 1
-Console.Write("Enter any three numbers of your choice:");
-int yourNumber1 = int.Parse(Console.ReadLine()!);
-int a = (yourNumber1 / 100) % 10;
-int b = (yourNumber1 / 10) % 10;
-int c = yourNumber1 % 10;
-int sum = a + b + c;
-Console.WriteLine ("The sum of the numbers is"+ " " + sum + ".");
+﻿// // Number 1
+// Console.Write("Enter any three numbers of your choice:");
+// int yourNumber1 = int.Parse(Console.ReadLine()!);
+// int a = (yourNumber1 / 100) % 10;
+// int b = (yourNumber1 / 10) % 10;
+// int c = yourNumber1 % 10;
+// int sum = a + b + c;
+// Console.WriteLine ("The sum of the numbers is"+ " " + sum + ".");
 
-// Number 2
-Console.Write("Enter the radius of the circle:");
-int radiusOfCircle = int.Parse(Console.ReadLine()!);
-double PiOfCircle = Math.PI;
-Console.WriteLine("The Area of the cirle is " + (PiOfCircle * radiusOfCircle * radiusOfCircle));
-Console.WriteLine("The Perimeter of the circle is " + (2 * PiOfCircle * radiusOfCircle));
+// // Number 2
+// Console.Write("Enter the radius of the circle:");
+// int radiusOfCircle = int.Parse(Console.ReadLine()!);
+// double PiOfCircle = Math.PI;
+// Console.WriteLine("The Area of the cirle is " + (PiOfCircle * radiusOfCircle * radiusOfCircle));
+// Console.WriteLine("The Perimeter of the circle is " + (2 * PiOfCircle * radiusOfCircle));
 
-// Number 3
-Console.Write("Enter the name of the name of the company:");
-string nameOfCompany = Console.ReadLine()!;
-Console.Write("Enter the address of the company:");
-string addressOfCompany = Console.ReadLine()!;
-Console.Write("Enter the phone number of the company:");
-double PhoneNumber = Double.Parse(Console.ReadLine()!);
-Console.Write("Enter the fax number of the company:");
-double faxNumber = double.Parse(Console.ReadLine()!);
-Console.Write("Enter the website of the company:");
-string websiteOfCompany = Console.ReadLine()!;
-Console.Write("======Now Enter the Information of the Manager.====== \nEnter the First name of the Manager:");
-string nameOfManager = Console.ReadLine()!;
-Console.WriteLine("Enter the Surname of the Manager:");
-string SurnameOfManager = Console.ReadLine()!;
-Console.Write("Enter the Phone Number of the Manager:");
-double numberOfManager = double.Parse(Console.ReadLine()!);
-Console.WriteLine("");
+// // Number 3
+// Console.Write("Enter the name of the name of the company:");
+// string nameOfCompany = Console.ReadLine()!;
+// Console.Write("Enter the address of the company:");
+// string addressOfCompany = Console.ReadLine()!;
+// Console.Write("Enter the phone number of the company:");
+// double PhoneNumber = Double.Parse(Console.ReadLine()!);
+// Console.Write("Enter the fax number of the company:");
+// double faxNumber = double.Parse(Console.ReadLine()!);
+// Console.Write("Enter the website of the company:");
+// string websiteOfCompany = Console.ReadLine()!;
+// Console.Write("======Now Enter the Information of the Manager.====== \nEnter the First name of the Manager:");
+// string nameOfManager = Console.ReadLine()!;
+// Console.WriteLine("Enter the Surname of the Manager:");
+// string SurnameOfManager = Console.ReadLine()!;
+// Console.Write("Enter the Phone Number of the Manager:");
+// double numberOfManager = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("");
 
-// Number 4
+// // Number 4
+// Console.WriteLine("|{0,-8:x}|\n|{0,-8:f2}|\n|{0,-8:f2}|",123,32/5,-32/5);
+
+// // Number 5
+// Console.Write("Enter the smaller number:");
+// int yourNumber2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the bigger number:");
+// int yourNumber3 = int.Parse(Console.ReadLine()!);
+
+// if (yourNumber2 < yourNumber3)
+// {
+//     for (int i = yourNumber2; i < yourNumber3; i++)
+//     {
+//         if (i % 5 == 0)
+//         {
+//         Console.WriteLine(i);
+//         }
+//     }
+// }
+
+// // Number 6
+// Console.Write("Enter the first number:");
+// int yourNumber4 = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the second number:");
+// int yourNumber5 = int.Parse(Console.ReadLine()!);
+// int unknown1 = Math.Max(yourNumber4,yourNumber5);
+// Console.WriteLine("The bigger number is "+ unknown1);
+
+// Number 7
+
+
+// Number 8
+
+
+// // Number 9
+// Console.Write("Enter a Number of your choice:");
+// int numOfChoice = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the number of times you want it to add:");
+// int addTimes = int.Parse(Console.ReadLine()!);
+// int d = numOfChoice;
+// for (int i = 1; i < addTimes; i++)
+// {
+//     d += numOfChoice;
+//     Console.WriteLine(d);
+// }
+
+// // Number 10
+// Console.Write("Enter the NUmber:");
+// int theNumber = int.Parse(Console.ReadLine()!);
+// for (int i = 1; i <= theNumber; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// Number 11
